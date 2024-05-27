80c7fe6ec0579cbfe59b8b4828ad56981fae7255
github_EmanTalal <emantalal.201999@gmail.com>
2024-05-27 21:07:46 +0300

    add

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("submit").addEventListener("click", function() {})
+let pp;
+
+function mycode(values) {
+    pp = Number(values) * 0.15;
+    document.getElementById("result").innerHTM = 'lkkkkk'
+    return pp;
+}
